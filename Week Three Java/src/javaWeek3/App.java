edf1794e56fd4587f46016d18ec37f91ceabf802
James Aiello <jamesaiello42@gmail.com>
2020-12-10 22:05:41 -0600

    Halfway through project

--- Week Three Java/src/javaWeek3/App.java
+++ Week Three Java/src/javaWeek3/App.java
@@ -1,13 +1,15 @@
-package javaWeek2;
+package javaWeek3;
 
 public class App {
 	public static void main(String[] args) {
 		// Ask about adding value to 
 		int ages[] = {3, 9, 23, 64, 2, 8, 21, 28, 93};
 		String names[] = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+		int nameLengths[] = new int[names.length];
 		
 		double avgAge = 0;
 		double letterNumAvg = 0;
+		int nameLengthSum = 0;
 		
 		System.out.println(ages[ages.length - 1] - ages[0]);
 		
@@ -26,5 +28,25 @@ public class App {
 		
 		System.out.println(letterNumAvg);
 		
+		for (int i = 0; i < names.length; i++)
+			System.out.print(names[i] + " ");
+		
+		for (int i = 0; i < names.length; i++)
+			nameLengths[i] = names[i].length();
+		
+		// Calculate the average of nameLengths
+		for (int i = 0; i < nameLengths.length; i++)
+			nameLengthSum += nameLengths[i]; 
+
+		System.out.println("\n" + nameLengthSum);
 	}
+	
+	public String printNTimes(String word, int n) {
+		String concatWord = null;
+		
+		for (int i = 0; i < n; i++)
+			concatWord += word;
+		return concatWord;
+	}
+	
 }
